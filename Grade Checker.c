61b8cccdb6948b00cfbbd677a109836e54a12dfa
shantanu <i.am.shantanu07@gmail.com>
2025-09-01 20:31:47 +0530

    Update Grade Checker.c

--- Grade Checker.c
+++ Grade Checker.c
@@ -1,29 +1,31 @@
 #include<stdio.h>
-#include<math.h>
 
-int main(){
+int main() {
+    printf("Welcome to the GRADE CHECKER\n");
     int marks;
     scanf("%d", &marks);
-    if(100>=marks && marks>=90){
+
+    if (marks >= 90 && marks <= 100) {
         printf("your grade is A");
     }
-        else if (90>=marks && marks>=80){
-            printf("youre grade is B");
-        }
-            else if (80>=marks && marks>=70){
-                printf("your grade is C");
-            }
-                else if (70>=marks  && marks>=60){
-                    printf("your grade is D");
-                }
-                    else if (60>=marks  && marks>=50){
-                        printf("your grade is E");
-                    }
-                        else if (50>marks){    
-                        printf("you are FAIL");
-                        }
-    else{
+    else if (marks >= 80 && marks < 90) {
+        printf("your grade is B");
+    }
+    else if (marks >= 70 && marks < 80) {
+        printf("your grade is C");
+    }
+    else if (marks >= 60 && marks < 70) {
+        printf("your grade is D");
+    }
+    else if (marks >= 50 && marks < 60) {
+        printf("your grade is E");
+    }
+    else if (marks < 50 && marks >= 0) {
+        printf("you are FAIL");
+    }
+    else {
         printf("invalid input");
     }
+    printf("Thankyou for using the GRADE CHECKER\nMade by Shantanu");
     return 0;
 }
